1bff03e3ade7bde48cb844d06c9220ca07ef8756
Gabrielle Ulisses <82672178+gabi-ulisses@users.noreply.github.com>
2025-01-20 20:20:49 -0300

    Atualizar o JogoDasPilhas.c

--- Estudos/exercicios/JogoDasPilhas.c
+++ Estudos/exercicios/JogoDasPilhas.c
@@ -35,49 +35,51 @@ void destruirPilha(Pilha* pilha) {
     free(pilha->elementos);
 }
 
+// Função que tenta remover uma combinação de cartas
 int podeRemover(Pilha* pilha1, Pilha* pilha2, Pilha* pilha3) {
     int t1 = topo(pilha1), t2 = topo(pilha2), t3 = topo(pilha3);
     
-    // Verifica se alguma combinação de cartas no topo é múltiplo de 3
-    if (t1 != -1 && (t1 % 3 == 0)) return 1;
-    if (t2 != -1 && (t2 % 3 == 0)) return 1;
-    if (t3 != -1 && (t3 % 3 == 0)) return 1;
-    if (t1 != -1 && t2 != -1 && (t1 + t2) % 3 == 0) return 1;
-    if (t2 != -1 && t3 != -1 && (t2 + t3) % 3 == 0) return 1;
-    if (t1 != -1 && t2 != -1 && t3 != -1 && (t1 + t2 + t3) % 3 == 0) return 1;
+    // Verifica se a soma das cartas no topo é múltiplo de 3
+    if (t1 != -1 && (t1 % 3 == 0)) {
+        desempilhar(pilha1);
+        return 1;
+    }
+    if (t2 != -1 && (t2 % 3 == 0)) {
+        desempilhar(pilha2);
+        return 1;
+    }
+    if (t3 != -1 && (t3 % 3 == 0)) {
+        desempilhar(pilha3);
+        return 1;
+    }
+    if (t1 != -1 && t2 != -1 && (t1 + t2) % 3 == 0) {
+        desempilhar(pilha1);
+        desempilhar(pilha2);
+        return 1;
+    }
+    if (t2 != -1 && t3 != -1 && (t2 + t3) % 3 == 0) {
+        desempilhar(pilha2);
+        desempilhar(pilha3);
+        return 1;
+    }
+    if (t1 != -1 && t2 != -1 && t3 != -1 && (t1 + t2 + t3) % 3 == 0) {
+        desempilhar(pilha1);
+        desempilhar(pilha2);
+        desempilhar(pilha3);
+        return 1;
+    }
     
     return 0;
 }
 
 int jogoVencido(Pilha* pilha1, Pilha* pilha2, Pilha* pilha3, int N) {
-    while (N > 0) {
-        if (podeRemover(pilha1, pilha2, pilha3)) {
-            int t1 = topo(pilha1), t2 = topo(pilha2), t3 = topo(pilha3);
-            
-            // Prioridade para retirar as cartas do topo se for múltiplo de 3
-            if (t1 != -1 && t1 % 3 == 0) {
-                desempilhar(pilha1);
-            } else if (t2 != -1 && t2 % 3 == 0) {
-                desempilhar(pilha2);
-            } else if (t3 != -1 && t3 % 3 == 0) {
-                desempilhar(pilha3);
-            } else if (t1 != -1 && t2 != -1 && (t1 + t2) % 3 == 0) {
-                desempilhar(pilha1);
-                desempilhar(pilha2);
-            } else if (t2 != -1 && t3 != -1 && (t2 + t3) % 3 == 0) {
-                desempilhar(pilha2);
-                desempilhar(pilha3);
-            } else if (t1 != -1 && t2 != -1 && t3 != -1 && (t1 + t2 + t3) % 3 == 0) {
-                desempilhar(pilha1);
-                desempilhar(pilha2);
-                desempilhar(pilha3);
-            }
-            N--;
-        } else {
-            return 0;
+    // Enquanto houver cartas nas pilhas, tenta remover as combinações válidas
+    while (!vazia(pilha1) || !vazia(pilha2) || !vazia(pilha3)) {
+        if (!podeRemover(pilha1, pilha2, pilha3)) {
+            return 0; // Não foi possível remover cartas, perdeu
         }
     }
-    return 1;
+    return 1; // Se todas as cartas foram removidas
 }
 
 int main() {
@@ -91,22 +93,3 @@ int main() {
         
         for (int i = 0; i < N; i++) {
             int A, B, C;
-            scanf("%d %d %d", &A, &B, &C);
-            empilhar(&pilha1, A);
-            empilhar(&pilha2, B);
-            empilhar(&pilha3, C);
-        }
-        
-        if (jogoVencido(&pilha1, &pilha2, &pilha3, N)) {
-            printf("1\n");
-        } else {
-            printf("0\n");
-        }
-
-        destruirPilha(&pilha1);
-        destruirPilha(&pilha2);
-        destruirPilha(&pilha3);
-    }
-
-    return 0;
-}
