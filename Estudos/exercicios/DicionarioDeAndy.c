4ee1b6e9c42e5cf249bf96156d3167081ef27e6a
Gabrielle Ulisses <82672178+gabi-ulisses@users.noreply.github.com>
2025-01-20 19:38:55 -0300

    Atualizar o exercicios

---
+++ Estudos/exercicios/DicionarioDeAndy.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+void processarString(char *str) {
+    int j = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (isalpha(str[i])) {
+            str[j] = tolower(str[i]);
+            j++;
+        } else {
+            str[j] = ' ';
+            j++;
+        }
+    }
+    str[j] = '\0';
+}
+
+void ordenarPalavras(char palavras[][50], int quantidade) {
+    char temp[50];
+    for (int i = 0; i < quantidade - 1; i++) {
+        for (int j = i + 1; j < quantidade; j++) {
+            if (strcmp(palavras[i], palavras[j]) > 0) {
+                strcpy(temp, palavras[i]);
+                strcpy(palavras[i], palavras[j]);
+                strcpy(palavras[j], temp);
+            }
+        }
+    }
+}
+
+int main() {
+    char entrada[10005];
+    char palavras[1000][50];
+    int quantidadePalavras = 0;
+
+    while (scanf("%s", entrada) == 1) {
+        processarString(entrada);
+
+        char *token = strtok(entrada, " ");
+        while (token != NULL) {
+            int encontrada = 0;
+
+            for (int i = 0; i < quantidadePalavras; i++) {
+                if (strcmp(palavras[i], token) == 0) {
+                    encontrada = 1;
+                    break;
+                }
+            }
+
+            if (encontrada == 0) {
+                strcpy(palavras[quantidadePalavras], token);
+                quantidadePalavras++;
+            }
+
+            token = strtok(NULL, " ");
+        }
+    }
+
+    ordenarPalavras(palavras, quantidadePalavras);
+
+    for (int i = 0; i < quantidadePalavras; i++) {
+        printf("%s\n", palavras[i]);
+    }
+
+    return 0;
+}
