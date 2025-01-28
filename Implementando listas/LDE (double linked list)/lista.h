45829515ec814e1f12b0ddfb8f39b40a8305b69b
gabi-ulisses <gabrielle.ulisses@aluno.ifsp.edu.br>
2025-01-27 22:06:15 -0300

    Implementing "LDE (double linked list)"

---
+++ Implementando listas/LDE (double linked list)/lista.h
@@ -0,0 +1,16 @@
+#include "node.h"
+#include <stdbool.h>
+
+typedef struct linked_list* List;
+
+List create();
+
+void destroy(List list);
+
+bool insert(List list, Info element);
+
+bool insertAtBeggining(List list, Info element);
+
+bool isEmpty(List list);
+
+Info getIntoAt(List list, int position);
---
+++ Implementando listas/LDE (double linked list)/listaImpl.c
@@ -0,0 +1,107 @@
+#include "lista.h"
+
+struct linked_list{
+    NodePtr head;
+};
+
+List create(){
+    List list = malloc(sizeof(struct linked_list));
+    if (list != NULL){
+        list->head = NULL;
+    }
+    return list;
+}
+
+void destroy(List list){
+    if (list != NULL){
+        NodePtr rip = list->head; // Primeiro nó
+        setNext(getPrevious(rip), NULL);
+        while (rip != NULL){
+           NodePtr dead = rip;
+           rip = getNext(rip);
+           destroyNode(dead);
+        }
+        free(list);
+    }
+}
+
+bool insert(List list, Info element){
+    bool result = false;
+
+    if (list != NULL) {
+        NodePtr node = createNode();
+        setInfo(node, element);
+
+        if (isEmpty(list)){
+            list->head = node;
+            setNext(node, node);
+            setPrevious(node, node);
+            result = true;
+        }else {
+            NodePtr last = getPrevious(list->head);
+            setNext(node, list->head);
+            setPrevious(list->head, node);
+            setNext(last, node);
+            setPrevious(node, last);
+        }
+        result = true;
+    }
+    return result;
+}
+
+bool insertAtBeggining(List list, Info element){
+    bool result = false;
+
+    if (list != NULL) {
+        NodePtr node = createNode();
+        setInfo(node, element);
+
+        if (isEmpty(list)){
+            list->head = node;
+            setNext(node, node);
+            setPrevious(node, node);
+            result = true;
+        }else {
+            NodePtr last = getPrevious(list->head);
+            setNext(node, list->head);
+            setPrevious(list->head, node);
+            setNext(last, node);
+            setPrevious(node, last);
+            list->head = node;
+        }
+        return true;
+    }
+    return false;
+}
+
+bool isEmpty(List list){
+    bool empty = false;
+    if (list->head == NULL){
+       empty = true;
+    }
+    return empty;
+}
+
+Info getIntoAt(List list, int position){
+    if (list != NULL){
+        if (!isEmpty(list)){
+            if (position >= 0){
+                int counter = 0;
+                NodePtr walker = list->head;
+                while (counter < position && getNext(walker) != list->head){
+                    walker = getNext(walker);
+                    counter += 1;
+                }
+                if (counter == position){
+                    return getInfo(walker);
+                }else{
+                    return NULL;
+                }
+                
+            }
+            
+        }
+        
+    }
+    return NULL;
+}
